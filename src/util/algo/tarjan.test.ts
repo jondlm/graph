201cad63ffe381a0c995508fa732adcf324f110f
Jon de la Motte <jdlm@stripe.com>
2023-06-12 17:14:53 -0700

    add tarjan's algorithm for finding cycles

--- src/util/algo/tarjan.test.ts
+++ src/util/algo/tarjan.test.ts
@@ -1,6 +1,82 @@
-import assert from "node:assert";
-import test from "node:test";
+import assert from "node:assert/strict";
+import test, { describe } from "node:test";
+import { adjacencyList, tarjan } from "./tarjan";
 
-test("test", () => {
-  assert(true);
+describe("tarjan", () => {
+  test("finds a small cycle", () => {
+    const graph = {
+      nodes: [
+        { id: "a", label: "", edges: ["b"] },
+        { id: "b", label: "", edges: ["c", "a"] },
+        { id: "c", label: "", edges: [] },
+      ],
+    };
+
+    assert.deepEqual(tarjan(graph), [["a", "b"]]);
+  });
+
+  test("finds all cycles", () => {
+    const graph = {
+      nodes: [
+        { id: "aa", label: "", edges: ["ba", "bb", "bc"] },
+        { id: "ba", label: "", edges: ["ca", "cb"] },
+        { id: "bb", label: "", edges: ["cc", "cd"] },
+        { id: "bc", label: "", edges: ["ce", "cf"] },
+        { id: "ca", label: "", edges: [] },
+        { id: "cb", label: "", edges: ["cc"] }, // cycle
+        { id: "cc", label: "", edges: ["cb"] }, // cycle
+        { id: "cd", label: "", edges: ["aa"] }, // cycle
+        { id: "ce", label: "", edges: [] },
+        { id: "cf", label: "", edges: [] },
+      ],
+    };
+
+    assert.deepEqual(tarjan(graph), [
+      ["aa", "bb", "cd"],
+      ["cb", "cc"],
+    ]);
+  });
+
+  test("no cycle", () => {
+    const graph = {
+      nodes: [
+        { id: "aa", label: "", edges: ["ba", "bb", "bc"] },
+        { id: "ba", label: "", edges: ["ca", "cb"] },
+        { id: "bb", label: "", edges: ["cc", "cd"] },
+        { id: "bc", label: "", edges: ["ce", "cf"] },
+        { id: "ca", label: "", edges: [] },
+        { id: "cb", label: "", edges: [] },
+        { id: "cc", label: "", edges: [] },
+        { id: "cd", label: "", edges: [] },
+        { id: "ce", label: "", edges: [] },
+        { id: "cf", label: "", edges: [] },
+      ],
+    };
+
+    assert.deepEqual(tarjan(graph), []);
+  });
+
+  describe("adjacencyList", () => {
+    test("basic", () => {
+      const graph = {
+        nodes: [
+          {
+            id: "a",
+            label: "a",
+            edges: ["b"],
+          },
+          {
+            id: "b",
+            label: "b",
+            edges: ["a"],
+          },
+        ],
+      };
+
+      assert.deepEqual(adjacencyList(graph)[0], {
+        0: [1],
+        1: [0],
+      });
+    });
+  });
 });
--- src/util/algo/tarjan.ts
+++ src/util/algo/tarjan.ts
@@ -1,40 +1,99 @@
 import type { Id, GraphData, Module } from "../types";
 
-function tarjan(input: GraphData) {
-  const modulesById = input.nodes.reduce((acc, node) => {
-    acc.set(node.id, node);
-    return acc;
-  }, new Map());
-  const scc: Array<Array<Id>> = [];
-  const stack: Array<Id> = [];
-  const visitied = new Set<string | number>(); // by id
+export function tarjan(input: GraphData) {
+  const UNVISITED = -1;
+  const n = input.nodes.length;
+  const [g, indexToId] = adjacencyList(input);
 
-  const traverse = (node: Module) => {
-    visitied.add(node.id);
-    stack.push(node.id);
+  let id = 0;
 
-    if (node.edges != null && node.edges.length > 0) {
-      for (const edge of node.edges) {
-        if (!visitied.has(edge)) {
-          traverse(modulesById.get(edge));
-        } else if (stack.includes(edge)) {
-          const sccNode = stack.pop()!;
-          const sccComponent = [sccNode];
-          while (stack[stack.length - 1] !== edge) {
-            sccComponent.push(stack.pop()!);
-          }
-          sccComponent.push(stack.pop()!);
-          scc.push(sccComponent);
-        }
+  const ids = Array(n).fill(UNVISITED);
+  const low = Array(n).fill(0);
+  const onStack = Array(n).fill(false);
+  const stack: number[] = [];
+
+  const dfs = (at: number) => {
+    stack.push(at);
+    onStack[at] = true;
+    ids[at] = id;
+    low[at] = id;
+    id += 1;
+
+    // Visit all neighbors and min low-link on callback
+    for (let to of g[at]) {
+      // v = at
+      // w = to
+      if (ids[to] === UNVISITED) {
+        dfs(to);
+        low[at] = Math.min(low[at], low[to]);
+      } else if (onStack[to] === true) {
+        low[at] = Math.min(low[at], ids[to]);
       }
+    }
+
+    // After visiting all `at` neighbors if we're at the start of an SCC, empty
+    // the seen stack until we back to the start of the SCC.
+    if (ids[at] === low[at]) {
+      let node;
+
+      do {
+        node = stack.pop()!;
+        onStack[node] = false;
+        low[node] = ids[at];
+      } while (node !== at);
     }
   };
 
-  for (const node of input.nodes) {
-    if (!visitied.has(node.id)) {
-      traverse(node);
+  for (let i = 0; i < n; i++) {
+    if (ids[i] === UNVISITED) {
+      dfs(i);
     }
   }
 
-  return scc;
+  // This transformed the `low` array into groupings of cycles. If multiple
+  // entries share the same "lowlink" value then they are part of an SCC.
+  const cyclesIndex: { [n: number]: number[] } = {};
+  for (let i = 0; i < n; i++) {
+    if (cyclesIndex[low[i]] == null) {
+      cyclesIndex[low[i]] = [];
+    }
+
+    cyclesIndex[low[i]].push(i);
+  }
+
+  const cycles: Id[][] = [];
+  for (const [, indexes] of Object.entries(cyclesIndex)) {
+    // With tarjan's, cycles are only SCCs with more than 1 node
+    if (indexes.length > 1) {
+      // Map the indexes back to the user-provided IDs
+      cycles.push(indexes.map((i) => indexToId[i]));
+    }
+  }
+
+  return cycles;
+}
+
+export function adjacencyList(
+  input: GraphData
+): [{ [i: number]: number[] }, { [n: number]: Id }] {
+  const list: { [i: number]: number[] } = {};
+
+  const idToIndex: { [id: Id]: number } = {};
+  const indexToId: { [n: number]: Id } = {};
+
+  input.nodes.forEach((node, i) => {
+    idToIndex[node.id] = i;
+    indexToId[i] = node.id;
+  });
+
+  for (let i = 0; i < input.nodes.length; i++) {
+    const node = input.nodes[i];
+    list[i] = [];
+
+    for (let edge of node.edges ?? []) {
+      list[i].push(idToIndex[edge]);
+    }
+  }
+
+  return [list, indexToId];
 }
